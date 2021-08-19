bc8177bf92105a1f2bb771b230d00b08d59068f9
Alexander Gård <alex.gaard@hotmail.com>
2021-08-19 09:21:26 +0200

    Satt opp internal routes

---
+++ src/routes/internal.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+export const setupInternalRoutes = (app: express.Application) => {
+	app.get('/internal/ready', (req, res) => {
+		res.send('ready');
+	});
+
+	app.get('/internal/alive', (req, res) => {
+		res.send('alive');
+	});
+};
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
 import { logger } from './logger';
+import { setupInternalRoutes } from './routes/internal';
 
 const app: express.Application = express();
 
 async function startServer() {
 	logger.info('Starting auth-proxy server...');
+
+	setupInternalRoutes(app);
 
 	app.listen(8080, () => logger.info('Server started successfully'));
 }
