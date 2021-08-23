a3710e6c5fd74164cb59efbd3ee3c887a738cce9
Alexander Gård <alex.gaard@hotmail.com>
2021-08-23 15:02:27 +0200

    Delte opp in-memory-store for å skape mindre konflikter mellom lagret data

--- src/client/in-memory-session-store.ts
+++ src/client/in-memory-session-store.ts
@@ -1,43 +1,51 @@
 import { TokenSet } from 'openid-client';
 import { LoginState, SessionStore } from './session-store';
 
-const store: { [key: string]: any; } = {};
+const store: {
+	loginState: { [key: string]: any;  }
+	idProviderSession: { [key: string]: string; },
+	sessionData: { [key: string]: any; }
+} = {
+	loginState: {},
+	idProviderSession: {},
+	sessionData: {}
+};
 
 export const inMemorySessionStore: SessionStore = {
 	getLoginState(id: string): Promise<LoginState | undefined> {
-		return Promise.resolve(store[id]);
+		return Promise.resolve(store.loginState[id]);
 	},
 	setLoginState(id: string, loginState: LoginState): Promise<void> {
-		store[id] = loginState;
+		store.loginState[id] = loginState;
 		return Promise.resolve();
 	},
 	destroyLoginState(id: string): Promise<void> {
-		store[id] = undefined;
+		store.loginState[id] = undefined;
 		return Promise.resolve();
 	},
 
 
 	getLogoutSessionId(oidcSessionId: string): Promise<string | undefined> {
-		return Promise.resolve(store[oidcSessionId]);
+		return Promise.resolve(store.idProviderSession[oidcSessionId]);
 	},
 	setLogoutSessionId(oidcSessionId: string, sessionId: string): Promise<void> {
-		store[oidcSessionId] = sessionId;
+		store.idProviderSession[oidcSessionId] = sessionId;
 		return Promise.resolve(undefined);
 	},
+	// TODO: Destroy logout session id
 
 
 	getUserTokenSet(sessionId: string): Promise<TokenSet | undefined> {
-		return Promise.resolve(store[sessionId]?.tokenSet);
+		return Promise.resolve(store.sessionData[sessionId]?.tokenSet);
 	},
 	setUserTokenSet(sessionId: string, tokenSet: TokenSet): Promise<void> {
-		store[sessionId] = {};
-		store[sessionId].tokenSet = tokenSet;
+		store.sessionData[sessionId] = { tokenSet };
 		return Promise.resolve();
 	},
 
 
 	destroySessionData(sessionId: string): Promise<void> {
-		store[sessionId] = undefined;
+		store.sessionData[sessionId] = undefined;
 		return Promise.resolve();
 	},
 };
