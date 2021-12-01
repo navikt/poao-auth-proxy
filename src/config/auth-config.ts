33270a152e79782134f443a4b7c8fde775f2f1a2
Alexander Gård <alex.gaard@hotmail.com>
2021-12-01 16:02:03 +0100

    Fjern ubrukt token refresh config

--- src/config/auth-config.ts
+++ src/config/auth-config.ts
@@ -4,10 +4,6 @@ import { assert, strToBoolean, strToEnum, strToNumber } from '../utils';
 import { logger } from '../utils/logger';
 import { JsonConfig } from './app-config-resolver';
 
-const DEFAULT_AUTH_ENABLE_REFRESH = false;
-
-const DEFAULT_AUTH_REFRESH_ALLOWED_WITHIN_SECONDS = 43_200; // 12 hours
-
 export enum LoginProvider {
 	ID_PORTEN = 'ID_PORTEN',
 	AZURE_AD = 'AZURE_AD',
@@ -18,8 +14,6 @@ export interface AuthConfig {
 	discoveryUrl: string;
 	clientId: string;
 	privateJwk: string;
-	enableRefresh: boolean;                 // If true then poao-auth-proxy will try to refresh expired access_tokens
-	refreshAllowedWithinSeconds: number;    // Specifies how long after initial login the user is allowed to refresh their tokens
 	tokenX?: TokenXConfig;
 }
 
@@ -36,8 +30,8 @@ export interface TokenXConfig {
 }
 
 export const logAuthConfig = (config: AuthConfig): void => {
-	const { loginProvider, discoveryUrl, clientId, enableRefresh } = config;
-	logger.info(`Auth config: authProvider=${loginProvider} discoveryUrl=${discoveryUrl} clientId=${clientId} enableRefresh=${enableRefresh}`);
+	const { loginProvider, discoveryUrl, clientId } = config;
+	logger.info(`Auth config: authProvider=${loginProvider} discoveryUrl=${discoveryUrl} clientId=${clientId}`);
 };
 
 export const resolveAuthConfig = (jsonConfig: JsonConfig | undefined): AuthConfig => {
@@ -45,14 +39,6 @@ export const resolveAuthConfig = (jsonConfig: JsonConfig | undefined): AuthConfi
 	const authConfigFromJson = resolveAuthConfigFromJson(jsonConfig);
 
 	let authConfig: Partial<AuthConfig> = merge({}, authConfigFromEnv, authConfigFromJson);
-
-	if (authConfig.enableRefresh == null) {
-		authConfig.enableRefresh = DEFAULT_AUTH_ENABLE_REFRESH;
-	}
-
-	if (authConfig.refreshAllowedWithinSeconds == null) {
-		authConfig.refreshAllowedWithinSeconds = DEFAULT_AUTH_REFRESH_ALLOWED_WITHIN_SECONDS;
-	}
 
 	if (authConfig.loginProvider === LoginProvider.AZURE_AD) {
 		const loginProviderConfig = resolveAzureAdLoginProviderConfig();
@@ -71,8 +57,6 @@ export const resolveAuthConfig = (jsonConfig: JsonConfig | undefined): AuthConfi
 const resolveAuthConfigFromEnvironment = (): Partial<AuthConfig> => {
 	return {
 		loginProvider: strToEnum(process.env.AUTH_LOGIN_PROVIDER, LoginProvider),
-		refreshAllowedWithinSeconds: strToNumber(process.env.AUTH_REFRESH_ALLOWED_WITHIN_SECONDS),
-		enableRefresh: strToBoolean(process.env.AUTH_ENABLE_REFRESH)
 	};
 };
 
@@ -81,8 +65,6 @@ const resolveAuthConfigFromJson = (jsonConfig: JsonConfig | undefined): Partial<
 
 	return {
 		loginProvider: strToEnum(jsonConfig.loginProvider, LoginProvider),
-		refreshAllowedWithinSeconds: jsonConfig.refreshAllowedWithinSeconds,
-		enableRefresh: strToBoolean(jsonConfig.enableRefresh)
 	};
 };
 
@@ -116,9 +98,6 @@ const validateAuthConfig = (config: Partial<AuthConfig>): AuthConfig => {
 	assert(config.discoveryUrl, `Auth 'discoveryUrl' is missing`);
 	assert(config.clientId, `Auth 'clientId' is missing`);
 	assert(config.privateJwk, `Auth 'privateJwk' is missing`);
-
-	assert(config.enableRefresh, `Auth 'enableRefresh' is missing`);
-	assert(config.refreshAllowedWithinSeconds, `Auth 'refreshAllowedWithinSeconds' is missing`);
 
 	if (config.loginProvider === LoginProvider.ID_PORTEN) {
 		assert(config.tokenX, `Auth 'tokenX' is missing`);
